897eab18ae1c792c40cd89fc260a315461c379df
Luis Fernando Calegari <calegariluisfernando@gmail.com>
2023-09-07 10:29:08 -0300

    Migrando para WSL 2

--- calegapetgui/lib/screens/login_screen.dart
+++ calegapetgui/lib/screens/login_screen.dart
@@ -22,107 +22,110 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(MyDefaultSettings.gutter),
-          child: SingleChildScrollView(
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(height: MyDefaultSettings.gutter),
-                  Column(
-                    children: [
-                      Container(
-                        padding:
-                            const EdgeInsets.all(MyDefaultSettings.gutter / 2),
-                        decoration: const BoxDecoration(
-                          color: MyDefaultSettings.logoColor,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(MyDefaultSettings.gutter),
-                            bottomRight:
-                                Radius.circular(MyDefaultSettings.gutter),
-                          ),
-                        ),
-                        child: const Icon(
-                          Icons.pets,
-                          color: Colors.white,
-                          size: 70,
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Pet',
-                            style: TextStyle(
-                              fontFamily: GoogleFonts.cormorant().fontFamily,
-                              fontSize: 50,
-                              color: MyDefaultSettings.logoColor,
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.all(MyDefaultSettings.gutter),
+            child: SingleChildScrollView(
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: MyDefaultSettings.gutter),
+                    Column(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(
+                              MyDefaultSettings.gutter / 2),
+                          decoration: const BoxDecoration(
+                            color: MyDefaultSettings.logoColor,
+                            borderRadius: BorderRadius.only(
+                              topLeft:
+                                  Radius.circular(MyDefaultSettings.gutter),
+                              bottomRight:
+                                  Radius.circular(MyDefaultSettings.gutter),
                             ),
                           ),
-                          Text(
-                            'Guard',
-                            style: TextStyle(
-                              fontFamily: GoogleFonts.cormorant().fontFamily,
-                              fontSize: 50,
-                              color: MyDefaultSettings.logoColor,
-                            ),
+                          child: const Icon(
+                            Icons.pets,
+                            color: Colors.white,
+                            size: 70,
                           ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: MyDefaultSettings.gutter),
-                  TextFormField(
-                    controller: emailController,
-                    decoration: const InputDecoration(
-                      labelText: 'E-mail',
-                      prefixIcon: Icon(Icons.email),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Calega-',
+                              style: TextStyle(
+                                fontFamily: GoogleFonts.cormorant().fontFamily,
+                                fontSize: 50,
+                                color: MyDefaultSettings.logoColor,
+                              ),
+                            ),
+                            Text(
+                              'Pet',
+                              style: TextStyle(
+                                fontFamily: GoogleFonts.cormorant().fontFamily,
+                                fontSize: 50,
+                                color: MyDefaultSettings.logoColor,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'O campo e-mail é obrigatório';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: MyDefaultSettings.gutter),
-                  TextFormField(
-                    controller: passwordController,
-                    decoration: InputDecoration(
-                      labelText: 'Senha',
-                      prefixIcon: const Icon(Icons.lock),
-                      suffixIcon: InkWell(
-                        onTap: togglePasswordVisibility,
-                        child: Icon(
-                          passwordIsVisible
-                              ? Icons.visibility_off
-                              : Icons.visibility,
+                    const SizedBox(height: MyDefaultSettings.gutter),
+                    TextFormField(
+                      controller: emailController,
+                      decoration: const InputDecoration(
+                        labelText: 'E-mail',
+                        prefixIcon: Icon(Icons.email),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'O campo e-mail é obrigatório';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: MyDefaultSettings.gutter),
+                    TextFormField(
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                        labelText: 'Senha',
+                        prefixIcon: const Icon(Icons.lock),
+                        suffixIcon: InkWell(
+                          onTap: togglePasswordVisibility,
+                          child: Icon(
+                            passwordIsVisible
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                          ),
                         ),
                       ),
+                      obscureText: !passwordIsVisible,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'O campo senha é obrigatório';
+                        }
+                        return null;
+                      },
                     ),
-                    obscureText: !passwordIsVisible,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'O campo senha é obrigatório';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: MyDefaultSettings.gutter * 2),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('Entrar'),
-                  ),
-                  const SizedBox(height: MyDefaultSettings.gutter),
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text('Não tem uma conta? Registre-se'),
-                  ),
-                ],
+                    const SizedBox(height: MyDefaultSettings.gutter * 2),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('Entrar'),
+                    ),
+                    const SizedBox(height: MyDefaultSettings.gutter),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text('Não tem uma conta? Registre-se'),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
